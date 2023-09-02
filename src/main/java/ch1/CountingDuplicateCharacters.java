550291e594ca3907e9599c3570b0c1b367fb0c43
dami <damixyz@outlook.com>
2023-09-03 00:23:59 +0100

    Address Counting Duplicate Chars

---
+++ src/main/java/ch1/CountingDuplicateCharacters.java
@@ -0,0 +1,28 @@
+package ch1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountingDuplicateCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(
+                countDuplicate("wiegehtsesihnen")
+        );
+        System.out.println(
+                countDuplicate("gutdankenschon")
+        );
+    }
+
+    private static Map<Character, Integer> countDuplicate(String value) {
+        Map<Character, Integer> result = new HashMap<>(256);
+
+        char[] charSeq = value.toCharArray();
+        for (char c :
+                charSeq) {
+            result.compute(c, (k, v) -> (v == null) ? 1 : ++v);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/ch1/readme.md
@@ -0,0 +1,13 @@
+### Counting Duplicate characters: 
+Write a program that counts duplicate characters from a given string.
+
+#### Note:
+
+* Program would accept `String` has a parameter.
+
+#### Initial Idea
+
+* Assume all characters fall within the **ASCII** tables, thus a value from 0-255
+* Create a Map filled with keys 0 to 255
+* Get the Char Value of each character 
+* Add one to map value anytime a char value matches the Map key.
