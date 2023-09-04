6ff52616a7f1d85156ddce841ef90d27c7b66ad6
dami <damixyz@outlook.com>
2023-09-04 05:05:02 +0100

    Added book solution Counting duplicate characters

--- src/main/java/ch1/readme.md
+++ src/main/java/ch1/readme.md
@@ -5,9 +5,15 @@ Write a program that counts duplicate characters from a given string.
 
 * Program would accept `String` has a parameter.
 
-#### Initial Idea
+#### Initial Idea:
 
 * Assume all characters fall within the **ASCII** tables, thus a value from 0-255
 * Create a Map filled with keys 0 to 255
 * Get the Char Value of each character 
 * Add one to map value anytime a char value matches the Map key.
+
+#### Initial Idea:
+
+Solution passed 🕺
+
+#### Suggested solution:
---
+++ src/main/java/ch1/solution/CountingDuplicateCharacters.java
@@ -0,0 +1,46 @@
+package ch1.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CountingDuplicateCharacters {
+
+    public Map<Character, Integer> countDuplicateCharactersForLoop(String str) {
+        Map<Character, Integer> result = new HashMap<>();
+
+        // or use for(char ch: str.toCharArray()) {...}
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+        return result;
+    }
+
+    public Map<Character, Long> countDuplicateCharactersStream(String str) {
+        return str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(k -> k, Collectors.counting()));
+    }
+
+    public Map<String, Integer> countDuplicateCharactersForLoopUniCode(String str) {
+        Map<String, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            int cp = str.codePointAt(i); // the representation of the Char value
+            String ch = String.valueOf(Character.toChars(cp)); // the representation of the String value
+            if (Character.charCount(cp) == 2) { // This check how much Char space this String occupies
+                // 2 means a surrogate pair then move to the next character
+                i++;
+            }
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+        return result;
+    }
+
+    public Map<String, Long> countDuplicateCharactersStreamUniCode(String str) {
+        return str.codePoints()
+                .mapToObj(c -> String.valueOf(Character.toChars(c)))
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    }
+}
